de021364c9b62056ee59431bdfac105d8d8580c3
dayrentjiang <dayrentjiang@gmail.com>
2026-01-25 02:43:31 +1100

    add routes to check region

---
+++ src/app/api/region/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export async function GET() {
+  return NextResponse.json({
+    region: process.env.VERCEL_REGION || "local",
+    env: process.env.VERCEL_ENV || "development",
+  });
+}
